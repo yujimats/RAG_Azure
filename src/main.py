d9ae1fd2fb286c3c5ed60baf3df7ef548aa6833e
yujimats <chanmatsu812@gmail.com>
2024-03-06 00:32:34 +0900

    複数ファイルの検索に対応 #2

--- src/main.py
+++ src/main.py
@@ -90,5 +90,81 @@ def main_onefile():
     query = '研究公募を実施する目的は何か?'
     print(chain({chain.question_key: query}))
 
+def main_multifiles():
+    target_path = os.path.join('data')
+    list_pdf = [f for f in os.listdir(target_path) if f.endswith('.pdf')]
+
+    embedding = get_embedding()
+
+    llm = AzureChatOpenAI(
+        api_version=os.environ['API_VERSION'],
+        azure_endpoint=os.environ['API_ENDPOINT'],
+        api_key=os.environ['API_KEYS'],
+        azure_deployment=os.environ['OPENAI_MODEL'],
+        temperature=0
+    )
+
+    vectorstore = Chroma(
+        collection_name='langchain',
+        embedding_function=embedding
+        # persist_directory='./chroma_db_multi/'
+    )
+
+    for pdf in list_pdf:
+        # path指定
+        filepath = os.path.join(target_path, pdf)
+        # ファイルをロード
+        loader = PyPDFLoader(filepath)
+        # langchain_core.documents.base.Document生成
+        documents = loader.load_and_split()
+
+        if os.path.exists('./chroma_db_multi/'):
+            vectorstore.add_documents(
+                documents=documents,
+                embedding=embedding,
+                persist_directory='./chroma_db_multi/'
+            )
+        else:
+            db = vectorstore.from_documents(
+                documents=documents,
+                embedding=embedding,
+                persist_directory='./chroma_db_multi/'
+            )
+
+    # filepath = os.path.join(target_path, list_pdf[0])
+    # loader = PyPDFLoader(filepath)
+    # documents = loader.load_and_split()
+    # db = vectorstore.from_documents(
+    #     documents=documents,
+    #     embedding=embedding,
+    #     persist_directory='./chroma_db_multi/'
+    # )
+
+    # filepath = os.path.join(target_path, list_pdf[1])
+    # loader = PyPDFLoader(filepath)
+    # documents = loader.load_and_split()
+    # vectorstore.add_documents(
+    #     documents=documents,
+    #     embedding=embedding,
+    #     persist_directory='./chroma_db_multi/'
+    # )
+
+    # 検索・参照先ファイルを出力するチェーンを作成
+    retriever = db.as_retriever()
+    chain = RetrievalQAWithSourcesChain.from_chain_type(
+        llm=llm,
+        chain_type="stuff",
+        retriever=retriever
+    )
+
+    # チェーン実行
+    query = '研究公募を実施する目的は何か？'
+    print(chain({chain.question_key: query}))
+
+    # チェーン実行
+    query = '日本における高精度遺伝子診断導入の早期立ち上げの目的で買収した企業はどこ？'
+    print(chain({chain.question_key: query}))
+
 if __name__=='__main__':
-    main_onefile()
+    # main_onefile()
+    main_multifiles()
